b736ca4db530eae0856f013a78c07fdeff483a5e
XOR19 <nils.h.emmerich@gmail.com>
2014-11-24 12:22:55 +0100

    Material to add

--- src/equipmentcraft/EquipmentCraft.java
+++ src/equipmentcraft/EquipmentCraft.java
@@ -45,7 +45,7 @@ public class EquipmentCraft {
 		
 		/*
 		 * Maybe to add:
-		 * Bone, Paper, Brick, Nether Brick, Flint Materials
+		 * Bone, Paper, Brick, Nether Brick, Flint, String Materials
 		 */
 		
 		Material[] materials = {wood, stone, iron, gold, diamond, emerald};
